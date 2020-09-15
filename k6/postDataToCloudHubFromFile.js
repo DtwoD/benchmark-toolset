64a9eaa1280d22480cf4599c5934bde85a89d724
sms-hceballos <hceballos@sms-sudamerica.com>
2020-09-15 11:46:30 -0300

    added some changes in K6 test

--- k6/postDataToCloudHubFromFile.js
+++ k6/postDataToCloudHubFromFile.js
@@ -1,13 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-let ediX12File = open('input-files/small-file-x12-004010-210.edi');
+let ediX12File = open('input-files/Large-EDI-219-10000.edi');
 
 export default function () {
     var data = {
         file: http.file(ediX12File, 'x12-test.edi'),
     };
 
-    var res = http.post('http://b2b-inbound-maps-x12-v3-0-0.us-e2.cloudhub.io/x12/210', data);
+    // var res = http.post('http://b2b-inbound-maps-x12-v3-0-0.us-e2.cloudhub.io/x12/210', ediX12File);
+    var res = http.post('http://localhost:8081/x12/210', ediX12File);
+
     sleep(3);
 }
