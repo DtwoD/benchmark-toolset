186798661141d1787392271801a112d4d857902e
Rodrigo Altamirano <raltamirano@mulesoft.com>
2020-12-03 09:52:39 -0300

    Write data to file as we go for larger files generation

--- x12-210-generator/generator.js
+++ x12-210-generator/generator.js
@@ -1,32 +1,36 @@
-const fs = require('fs');
-const utils = require('./utils');
-const myArgs = process.argv.slice(2);
-const folderToCopyFile = myArgs[1];
-const index = myArgs[0]?myArgs[0]:1;
+const fs = require('fs')
+const utils = require('./utils')
 
-var dir = './outputs';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-var stFragment = fs.readFileSync("templates/ST-FRAGMENT").toString();
-var append = "";
-for(let i = 1;i<=index;i++){
-    append+=(stFragment.split("{$fi}").join(i.toString().padStart(7, "0")))+"\n";
-}
+const myArgs = process.argv.slice(2)
+const folderToCopyFile = myArgs[1]
+const index = myArgs[0] ? myArgs[0] : 1
+const dir = './outputs'
+var outputFile = dir + "/message.edi";
 
-var stFragment = fs.readFileSync("templates/EDI-FRAGMENT").toString();
-fs.writeFileSync(dir+"/message.edi",stFragment.split("{$fr}").join(append).split("{$index}").join(index),{});
-let fileSieze = utils.getFileSize(`${dir}/message.edi`);
-console.log('\x1b[33m%s\x1b[0m',`Generated file with ${fileSieze}`);
+if (!fs.existsSync(dir))
+    fs.mkdirSync(dir)
+
+fs.writeFileSync(outputFile, fs.readFileSync("templates/EDI-START-FRAGMENT").toString())
+fs.appendFileSync(outputFile, fs.readFileSync("templates/GROUP-START-FRAGMENT").toString())
+
+const stFragmentTemplate = fs.readFileSync("templates/ST-FRAGMENT").toString()
+for(let i = 1; i <= index; i++)
+  fs.appendFileSync(outputFile, stFragmentTemplate.replace(/\{\$fi\}/g, i.toString().padStart(7, "0")))
+
+fs.appendFileSync(outputFile, fs.readFileSync("templates/GROUP-END-FRAGMENT").toString()
+  .replace(/\{\$index\}/g, index))
+fs.appendFileSync(outputFile, fs.readFileSync("templates/EDI-END-FRAGMENT").toString())
+
+let fileSize = utils.getFileSize(outputFile);
+console.log('\x1b[33m%s\x1b[0m',`Generated file with ${fileSize}`)
 
 if(folderToCopyFile){
-    if (!fs.existsSync(folderToCopyFile)){
-        fs.mkdirSync(folderToCopyFile);
-    }
-    let pathToCp = `${folderToCopyFile}/X12-${fileSieze}.edi`;
-    fs.copyFile(`${dir}/message.edi`, pathToCp, (err) => {
-        if (err) throw err;
-        console.log('\x1b[33m%s\x1b[0m',`Copied message.edi to ${pathToCp}`);
-    });
-}
+    if (!fs.existsSync(folderToCopyFile))
+        fs.mkdirSync(folderToCopyFile)
 
+    let pathToCp = `${folderToCopyFile}/X12-${fileSize}.edi`
+    fs.copyFile(outputFile, pathToCp, (err) => {
+        if (err) throw err;
+        console.log('\x1b[33m%s\x1b[0m',`Copied message.edi to ${pathToCp}`)
+    })
+}
